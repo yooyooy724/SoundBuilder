e494b9f72900a98a0bdc2292403f9807a6dd1865
yooyoo <48715626+yooyooy724@users.noreply.github.com>
2022-03-22 12:30:17 +0900

    rebuild playSound.cs

--- Assets/Scripts/SoundEditor/Center/PlaySound.cs
+++ Assets/Scripts/SoundEditor/Center/PlaySound.cs
@@ -7,14 +7,16 @@ using UniRx;
 public class PlaySound : MonoBehaviour
 {
 	[SerializeField] AudioSource audioSource;
-	[SerializeField] AudioClip sound;
-	[SerializeField] Button button;
+	[SerializeField] private AudioClip sound;
+    [SerializeField] Button button;
+	private PlaySoundInfomations SoundInfo;
 
 
     private void Start()
     {
+		SoundInfo = gameObject.GetComponent<PlaySoundInfomations>();
 
-		
+		this.ObserveEveryValueChanged(_ => _.SoundInfo.sound).Subscribe(_ => this.sound = _);
 
 		button.onClick.AddListener(() =>
 		{
--- Assets/Scripts/SoundEditor/Top&Bottom/SoundToggleManager.cs
+++ Assets/Scripts/SoundEditor/Top&Bottom/SoundToggleManager.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SoundToggleManager : MonoBehaviour
 {
+    [SerializeField] Toggle volumeToggle;
+    [SerializeField] Toggle speedToggle;
+
     // Start is called before the first frame update
     void Start()
     {
